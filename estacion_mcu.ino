a6b753d058d5be25cabe5a15d3a99fb66325daa6
Pablo Cabrera <binchmod777@gmail.comn>
2018-10-18 16:00:17 -0300

    movido DHT22 a pin 7. Dos redes Wifi

--- estacion_mcu.ino
+++ estacion_mcu.ino
@@ -4,10 +4,24 @@
 #include <Adafruit_BMP085.h>
 #include <DHTesp.h>
 #include <Wire.h>
+//#include <SPI.h>
+//#include <SD.h>
 #include <math.h>
 
-#define PIN_DHT D3
+#define READ_INTERVAL 30000
+#define CHANGE_NET_INTERVAL 10000
+
+#define PIN_DHT D7
 #define PIN_LDR A0
+#define PIN_SD_CS D0
+#define PIN_ERROR_LED D8
+
+#define WIFI_SSID "wifi_wemos"
+#define WIFI_PASSWORD "wifi_wemos"
+
+#define WIFI_ALT_SSID "unlu"
+#define WIFI_ALT_PASSWORD ""
+
 #define SEND_URL "http://serv.cidetic.unlu.edu.ar/estacionunlu/tiempo_real/cargar_datos.php"
 
 DHTesp dht;
@@ -16,74 +30,150 @@ float global_temp;
 float global_hum;
 
 void connect_wifi () {
-  WiFi.begin ("unlu", "");
-  
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay (500);
-    Serial.print(".");
-  }
-  Serial.println();
+	boolean prev_use_alt = false;
+	boolean use_alt = false;
 
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+	Serial.print ("\r\nBuscando red "); Serial.print (WIFI_SSID);
+	WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+	WiFi.reconnect ();
+	
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		use_alt = (((int) (millis() / CHANGE_NET_INTERVAL)) % 2) != 0;
+		if (use_alt != prev_use_alt) {
+			if (use_alt) {
+				Serial.print ("\r\nBuscando red "); Serial.print (WIFI_ALT_SSID);
+				WiFi.begin (WIFI_ALT_SSID, WIFI_ALT_PASSWORD);
+				WiFi.reconnect ();
+			} else {
+				Serial.print ("\r\nBuscando red "); Serial.print (WIFI_SSID);
+				WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+				WiFi.reconnect ();
+			}
+		}
+		prev_use_alt = use_alt;
+
+		digitalWrite (BUILTIN_LED, HIGH);
+		delay (250);
+		digitalWrite (BUILTIN_LED, LOW);
+		delay (250);
+		Serial.print(".");
+	}
+	Serial.println();
+
+	Serial.print("Connected, IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 
 void read_sensors (float *temp, float *temp_bmp, float *presion, float *hum, float *lum_perc) {
-  digitalWrite (BUILTIN_LED, LOW);
-  float input;
+	digitalWrite (BUILTIN_LED, HIGH);
+	float input;
 
-  input = dht.getHumidity ();
-  *hum = (isnan (input))? (*hum): input;
-  input = dht.getTemperature ();
-  *temp = (isnan (input))? (*temp): input;
-  int lum = analogRead (PIN_LDR);
-  *lum_perc = ((float) lum)/10.23;
-  *temp_bmp = bmp.readTemperature();
-  *presion = 0.01*bmp.readPressure();
-  
-   digitalWrite (BUILTIN_LED, HIGH);
+	input = dht.getTemperature ();
+	if (!isnan (input)) {
+		*temp = input;
+	}
+
+	input = dht.getHumidity ();
+	if (!isnan (input)) {
+		*hum = input;
+	}
+
+	int lum = analogRead (PIN_LDR);
+	*lum_perc = ((float) lum)/10.23;
+	*temp_bmp = bmp.readTemperature();
+	*presion = 0.01*bmp.readPressure();
+
+	*hum = (isnan (input))? (*hum): input;
+		 digitalWrite (BUILTIN_LED, LOW);
+}
+
+void notify_error (float *temp, float *temp_bmp, float *presion, float *hum, float *lum_perc) {
+	if ((*temp == -100) || (*hum == -100)) {
+		digitalWrite (PIN_ERROR_LED, HIGH);
+	} else {
+		digitalWrite (PIN_ERROR_LED, LOW);
+	}
+}
+
+void store_sd (float temp_dht, float temp_bmp, float presion, float hum, float lum_perc) {
+	/*char write_data[2048];
+	
+	sprintf (write_data, "%d.%d,%d.%d,%d.%d,%d.%d,%d.%d",
+		((int)temp_dht),((int)(temp_dht*100.0))%100,
+		((int) temp_bmp), ((int)(temp_bmp*100.0))%100,
+		((int)presion), ((int)(presion*100.0))%100,
+		((int)hum), ((int)(hum*100.0))%100,
+		((int)lum_perc), ((int)(lum_perc*100.0))%100
+	);
+	File sd_file = SD.open("ESTACION.CSV", FILE_WRITE);
+	if (!sd_file.available()) {
+		Serial.println ("File not available");
+	}
+	sd_file.println (write_data);
+	sd_file.close ();
+	*/
 }
 
 void send_to_server (float temp_dht, float temp_bmp, float presion, float hum, float lum_perc) {
-  char post_data[2048];
-  HTTPClient http;
-  http.begin (SEND_URL);
-  http.addHeader ("Content-Type", "application/x-www-form-urlencoded");
-  va_list ap;
-  sprintf (post_data, "temperatura_dht=%d.%d&temperatura_bmp=%d.%d&presion=%d.%d&humedad=%d.%d&luminosidad=%d.%d",
-    ((int)temp_dht),((int)(temp_dht*100.0))%100,
-    ((int) temp_bmp), ((int)(temp_bmp*100.0))%100,
-    ((int)presion), ((int)(presion*100.0))%100,
-    ((int)hum), ((int)(hum*100.0))%100,
-    ((int)lum_perc), ((int)(lum_perc*100.0))%100
-  );
-  Serial.print ("Enviando a servidor: ");
-  Serial.println (post_data);
-  http.POST ((unsigned char*) post_data, strlen (post_data));
-  http.end ();
+	digitalWrite (BUILTIN_LED, HIGH);
+	char post_data[2048];
+	HTTPClient http;
+	http.begin (SEND_URL);
+	http.addHeader ("Content-Type", "application/x-www-form-urlencoded");
+	sprintf (post_data, "temperatura_dht=%d.%d&temperatura_bmp=%d.%d&presion=%d.%d&humedad=%d.%d&luminosidad=%d.%d",
+		((int)temp_dht),((int)(temp_dht*100.0))%100,
+		((int) temp_bmp), ((int)(temp_bmp*100.0))%100,
+		((int)presion), ((int)(presion*100.0))%100,
+		((int)hum), ((int)(hum*100.0))%100,
+		((int)lum_perc), ((int)(lum_perc*100.0))%100
+	);
+	Serial.print ("Enviando a servidor: ");
+	Serial.println (post_data);
+	http.POST ((unsigned char*) post_data, strlen (post_data));
+	http.end ();
+	digitalWrite (BUILTIN_LED, LOW);
 }
 
 
 void setup() {
-  pinMode (BUILTIN_LED, OUTPUT);
-  pinMode (PIN_LDR, INPUT);
-  Serial.begin (9600);
-  digitalWrite (BUILTIN_LED, HIGH);
-  dht.setup (PIN_DHT, DHTesp::DHT22);
-  Wire.begin (D2, D1);
-  bmp.begin ();
-  connect_wifi ();
-  global_temp = -100.0;
-  global_hum = -100.0;
+	pinMode (BUILTIN_LED, OUTPUT);
+	pinMode (PIN_ERROR_LED, OUTPUT);
+	pinMode (PIN_DHT, INPUT);
+	pinMode (PIN_LDR, INPUT);
+
+	digitalWrite (BUILTIN_LED, HIGH);
+	digitalWrite (PIN_ERROR_LED, HIGH);
+
+	global_temp = -100.0;
+	global_hum = -100.0;
+	Serial.begin (9600);
+
+	/*if (SD.begin (PIN_SD_CS)) {
+		Serial.println ("SD Init OK");
+	}else {
+		Serial.println ("SD Init FAILED");
+	}*/
+
+	delay (500);
+	Wire.begin (D2, D1);
+	bmp.begin ();
+	delay (500);
+	digitalWrite (PIN_ERROR_LED, LOW);
+	digitalWrite (BUILTIN_LED, LOW);
+	delay (500);
+	connect_wifi ();
+	delay (500);
+	dht.setup (PIN_DHT, DHTesp::DHT22);
 }
 
 void loop() {
-  float lum_perc, temp_bmp, presion;
-  read_sensors (&global_temp, &temp_bmp, &presion, &global_hum, &lum_perc);
-  send_to_server (global_temp, temp_bmp, presion, global_hum, lum_perc);
-  delay (5000);
+	float lum_perc, temp_bmp, presion;
+	read_sensors (&global_temp, &temp_bmp, &presion, &global_hum, &lum_perc);
+	notify_error (&global_temp, &temp_bmp, &presion, &global_hum, &lum_perc);
+	//store_sd (global_temp, temp_bmp, presion, global_hum, lum_perc);
+	send_to_server (global_temp, temp_bmp, presion, global_hum, lum_perc);
+	delay (READ_INTERVAL);
 }
 
