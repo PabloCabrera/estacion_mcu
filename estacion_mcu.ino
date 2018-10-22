bf1ae304c485eda96a5dfaecafd4a8003a28983b
Pablo Cabrera <binchmod777@gmail.comn>
2018-10-22 13:54:13 -0300

    Integrados reloj y SD

--- estacion_mcu.ino
+++ estacion_mcu.ino
@@ -1,20 +1,28 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+// #include <Thread.h>
+// #include <ThreadController.h>
 
 #include <Adafruit_BMP085.h>
 #include <DHTesp.h>
 #include <Wire.h>
-//#include <SPI.h>
-//#include <SD.h>
+#include <SPI.h>
+#include <SD.h>
+#include <RTClib.h>
 #include <math.h>
 
-#define READ_INTERVAL 30000
+#define READ_INTERVAL 15000
 #define CHANGE_NET_INTERVAL 10000
 
-#define PIN_DHT D7
+#define PIN_DHT D0
 #define PIN_LDR A0
-#define PIN_SD_CS D0
+#define PIN_SCK D5
+#define PIN_MISO D6
+#define PIN_MOSI D7
+#define PIN_SDCS D3
 #define PIN_ERROR_LED D8
+
+#define TIMEZONE -3
 
 #define WIFI_SSID "wifi_wemos"
 #define WIFI_PASSWORD "wifi_wemos"
@@ -23,11 +31,15 @@
 #define WIFI_ALT_PASSWORD ""
 
 #define SEND_URL "http://serv.cidetic.unlu.edu.ar/estacionunlu/tiempo_real/cargar_datos.php"
+#define TIMESTAMP_URL "http://serv.cidetic.unlu.edu.ar/estacionunlu/tiempo_real/cargar_datos.php"
 
 DHTesp dht;
+RTC_DS1307 rtc;
 Adafruit_BMP085 bmp;
 float global_temp;
 float global_hum;
+
+// ThreadController threads;
 
 void connect_wifi () {
 	boolean prev_use_alt = false;
@@ -37,8 +49,7 @@ void connect_wifi () {
 	WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
 	WiFi.reconnect ();
 	
-	while (WiFi.status() != WL_CONNECTED)
-	{
+	while (WiFi.status() != WL_CONNECTED) {
 		use_alt = (((int) (millis() / CHANGE_NET_INTERVAL)) % 2) != 0;
 		if (use_alt != prev_use_alt) {
 			if (use_alt) {
@@ -98,22 +109,29 @@ void notify_error (float *temp, float *temp_bmp, float *presion, float *hum, flo
 }
 
 void store_sd (float temp_dht, float temp_bmp, float presion, float hum, float lum_perc) {
-	/*char write_data[2048];
+	char write_data[2048];
+	char *time_string = get_time_string ();
 	
-	sprintf (write_data, "%d.%d,%d.%d,%d.%d,%d.%d,%d.%d",
-		((int)temp_dht),((int)(temp_dht*100.0))%100,
+	SD.begin (PIN_SDCS);
+
+	sprintf (write_data, "%s,%d.%d,%d.%d,%d.%d,%d.%d,%d.%d",
+		time_string,
+		((int) temp_dht),((int)(temp_dht*100.0))%100,
 		((int) temp_bmp), ((int)(temp_bmp*100.0))%100,
-		((int)presion), ((int)(presion*100.0))%100,
-		((int)hum), ((int)(hum*100.0))%100,
-		((int)lum_perc), ((int)(lum_perc*100.0))%100
+		((int) presion), ((int)(presion*100.0))%100,
+		((int) hum), ((int)(hum*100.0))%100,
+		((int) lum_perc), ((int)(lum_perc*100.0))%100
 	);
 	File sd_file = SD.open("ESTACION.CSV", FILE_WRITE);
-	if (!sd_file.available()) {
-		Serial.println ("File not available");
-	}
-	sd_file.println (write_data);
+	byte written = sd_file.println (write_data);
 	sd_file.close ();
-	*/
+	if (written == 0) {
+		for (int i=0; i < 6;i++) {
+			digitalWrite (PIN_ERROR_LED, !digitalRead (PIN_ERROR_LED));
+			delay (100);
+		}
+	}
+	free (time_string);
 }
 
 void send_to_server (float temp_dht, float temp_bmp, float presion, float hum, float lum_perc) {
@@ -136,43 +154,89 @@ void send_to_server (float temp_dht, float temp_bmp, float presion, float hum, f
 	digitalWrite (BUILTIN_LED, LOW);
 }
 
+uint32_t get_remote_timestamp () {
+	unsigned long start_millis = millis ();
+	HTTPClient http;
+	http.begin (TIMESTAMP_URL);
+	Serial.print ("Consultado hora a servidor: ");
+	int response_code = http.GET ();
+	uint32_t timestamp;
+	if (response_code == 200) {
+		String response = http.getString ();
+		timestamp = response.toInt ();
+		unsigned long end_millis = millis ();
+		unsigned long adjust =  (((end_millis - start_millis)/2 + 500) / 1000);
+		adjust += (TIMEZONE) * 3600;
+		timestamp += (uint32_t) adjust;
+		Serial.println (timestamp);
+	} else {
+		timestamp = 0;
+	}
+	http.end ();
+	return timestamp;
+}
+
+void clock_sync () {
+	uint32_t timestamp = get_remote_timestamp ();
+	if (timestamp > 0) {
+		DateTime *now = new DateTime ((uint32_t) timestamp);
+		rtc.adjust (*now);
+		Serial.println ("HORA ESTABLECIDA");
+	} else {
+		Serial.println ("NO SE PUEDE CONSULTAR LA HORA DEL SERVIDOR");
+	}
+}
+
+char *get_time_string () {
+	char *time_string = (char*) malloc (20);
+	DateTime now = rtc.now ();
+	sprintf(time_string, "%04d-%02d-%02d %02d:%02d:%02d", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+	return time_string;
+}
 
 void setup() {
 	pinMode (BUILTIN_LED, OUTPUT);
 	pinMode (PIN_ERROR_LED, OUTPUT);
 	pinMode (PIN_DHT, INPUT);
 	pinMode (PIN_LDR, INPUT);
+	pinMode (PIN_SCK, OUTPUT);
+	pinMode (PIN_MISO, INPUT);
+	pinMode (PIN_MOSI, OUTPUT);
+	pinMode (PIN_SDCS, OUTPUT);
 
 	digitalWrite (BUILTIN_LED, HIGH);
 	digitalWrite (PIN_ERROR_LED, HIGH);
+
+	// threads = ThreadController ();
 
 	global_temp = -100.0;
 	global_hum = -100.0;
 	Serial.begin (9600);
 
-	/*if (SD.begin (PIN_SD_CS)) {
-		Serial.println ("SD Init OK");
-	}else {
-		Serial.println ("SD Init FAILED");
-	}*/
-
 	delay (500);
 	Wire.begin (D2, D1);
 	bmp.begin ();
+	rtc.begin ();
 	delay (500);
 	digitalWrite (PIN_ERROR_LED, LOW);
 	digitalWrite (BUILTIN_LED, LOW);
 	delay (500);
 	connect_wifi ();
+	clock_sync ();
 	delay (500);
 	dht.setup (PIN_DHT, DHTesp::DHT22);
 }
 
 void loop() {
 	float lum_perc, temp_bmp, presion;
+	
+	if (WiFi.status() != WL_CONNECTED) {
+		connect_wifi ();
+	}
+
 	read_sensors (&global_temp, &temp_bmp, &presion, &global_hum, &lum_perc);
 	notify_error (&global_temp, &temp_bmp, &presion, &global_hum, &lum_perc);
-	//store_sd (global_temp, temp_bmp, presion, global_hum, lum_perc);
+	store_sd (global_temp, temp_bmp, presion, global_hum, lum_perc);
 	send_to_server (global_temp, temp_bmp, presion, global_hum, lum_perc);
 	delay (READ_INTERVAL);
 }
